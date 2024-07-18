53000efe3f7de2f54b77ceeaed31e451f8add2fc
oykuky <oykuky001@gmail.com>
2024-07-19 01:05:02 +0300

    updated

---
+++ src/constants/options.jsx
@@ -0,0 +1,54 @@
+export const SelectBudgetOptions=[
+    {
+        id:1,
+        title:'Cheap',
+        desc:"Stay conscious of costs",
+        icon:'💵',
+    },
+    {
+        id:2,
+        title:'Moderate',
+        desc:"Keep cost on the average side",
+        icon:'💰',
+    },
+    {
+        id:3,
+        title:'Luxury',
+        desc:"Don't worry about cost",
+        icon:'💎',
+    },
+]
+
+export const SelectTravelList=[
+    {
+        id:1,
+        title:'Just Me',
+        desc:"A sole traveles",
+        icon:'🙋🏾‍♀️',
+        people:'1',
+    },
+    {
+        id:2,
+        title:'A couple',
+        desc:"Two travelers",
+        icon:'👫🏾',
+        people:'2',
+    },
+    {
+        id:3,
+        title:'Family',
+        desc:"A group of fun loving adv",
+        icon:'🏡',
+        people:'3 to 5 people',
+    },
+    {
+        id:4,
+        title:'Friends',
+        desc:"A bunch of thrill-seekers",
+        icon:'👩‍👩‍👦‍👦',
+        people:'5 to 12 people',
+    },
+]
+
+
+export const AI_PROMPT='Generate Travel Plan for Location : {location}'
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,20 +1,44 @@
-import ReactGoogleAutocomplete from "react-google-autocomplete"
-
+import React, { useState } from "react"
+import GooglePlacesAutocomplete from "react-google-places-autocomplete"
+import { Input } from "@/components/ui/input"
+import { SelectBudgetOptions } from "@/constants/options"
 
 function CreateTrip() {
+  const [place,setPlace]=useState()
   return (
     <div className="flex flex-col mt-12 sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5">
       <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
       <p className="mt-3 text-gray-600 text-xl">Just provide some basic information,and our trip planner will generate a customized itinerary based on your preferences.</p>
 
-      <div className="flex flex-col mt-20">
+      <div className="flex flex-col mt-20 gap-9">
+       <div>
         <label className="text-xl my-3 font-medium">What is destination of choice?</label>
-        <ReactGoogleAutocomplete 
-        apiKey=""
+          <GooglePlacesAutocomplete
+          apiKey={import.meta.env.VITE_GOOGLE_PLACES_API_KEY}
+          selectProps={{
+            place,
+            onChange:(v)=>{setPlace(v); console.log(v)}
+          }}
         />
-        <label className="text-xl my-3 font-medium">How many days are you planning your trip?</label>
-        <input type="text" name="trip-days"/>
-        <label className="text-xl my-3 font-medium">What is Your Budget?</label>
+       </div>
+        <div>
+          <label className="text-xl my-3 font-medium">How many days are you planning your trip?</label>
+          <Input placeholder={'ex.3'} type='number'/>
+        </div>
+
+        <div>
+          <label className="text-xl my-3 font-medium">What is Your Budget?</label>
+          <div className="grid grid-cols-3 gap-5 mt-5">
+            {SelectBudgetOptions.map((item,index)=>(
+              <div key={index} className="cursor-pointer p-4 border rounded-lg hover:shadow-lg">
+                <h2 className="text-4xl">{item.icon}</h2>
+                <h2 className="font-bold text-lg">{item.title}</h2>
+                <h2 className="text-sm text-gray-500">{item.desc}</h2>
+              </div>
+            ))}
+
+          </div>
+        </div>
       </div>
 
     </div>
@@ -22,4 +46,3 @@ function CreateTrip() {
 }
 
 export default CreateTrip
-// AIzaSyC4uqZiYTyD561ZkNU2PGrfBFH4pGf66sc
