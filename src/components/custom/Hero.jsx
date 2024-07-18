3a4a55629a617b11eb25a59dce08abc255047c5f
oykuky <oykuky001@gmail.com>
2024-07-18 23:48:48 +0300

    updated

--- src/components/custom/Hero.jsx
+++ src/components/custom/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from '../ui/button';
+import { Link } from 'react-router-dom';
 
 function Hero() {
   return (
@@ -8,7 +9,9 @@ function Hero() {
       className="font-extrabold text-[50px] text-center mt-16">
         <span className='text-[#f56551]'>Discover Your Next Adventure with AI:</span> <br></br> Personalized Itineraries at Your Fingertips</h1>
         <p className='text-xl text-gray-500 text-center'>Your personal trip planner and travel curator,creating custom itineraries tailored to your interests and budget.</p>
-        <Button>Get Started, It's Free.</Button>
+        <Link to={'/create-trip'}>
+          <Button>Get Started, It's Free.</Button>
+        </Link>
     </div>
   )
 }
--- src/create-trip/index.jsx
+++ src/create-trip/index.jsx
@@ -1,11 +1,25 @@
+import ReactGoogleAutocomplete from "react-google-autocomplete"
 
 
 function CreateTrip() {
   return (
-    <div>
-      het
+    <div className="flex flex-col mt-12 sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5">
+      <h2 className="font-bold text-3xl">Tell us your travel preferences</h2>
+      <p className="mt-3 text-gray-600 text-xl">Just provide some basic information,and our trip planner will generate a customized itinerary based on your preferences.</p>
+
+      <div className="flex flex-col mt-20">
+        <label className="text-xl my-3 font-medium">What is destination of choice?</label>
+        <ReactGoogleAutocomplete 
+        apiKey=""
+        />
+        <label className="text-xl my-3 font-medium">How many days are you planning your trip?</label>
+        <input type="text" name="trip-days"/>
+        <label className="text-xl my-3 font-medium">What is Your Budget?</label>
+      </div>
+
     </div>
   )
 }
 
 export default CreateTrip
+// AIzaSyC4uqZiYTyD561ZkNU2PGrfBFH4pGf66sc
