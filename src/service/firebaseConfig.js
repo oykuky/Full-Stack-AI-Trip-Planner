b1a68e761e481d2081fcde025a6087d833681684
oykuky <oykuky001@gmail.com>
2024-07-23 15:28:14 +0300

    added

--- src/service/firebaseConfig.js
+++ src/service/firebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import {getFirebase} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
