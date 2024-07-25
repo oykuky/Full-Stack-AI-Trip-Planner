853f81ff2e0c28df0d230e41a43823abc41fd7bf
oykuky <oykuky001@gmail.com>
2024-07-26 00:23:07 +0300

    Footer added

---
+++ src/view-trip/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='my-6'>
+      <h2 className='text-gray-400 text-center'>Created by Öykü Kaya Ai Travel Planner App    </h2>
+    </div>
+  )
+}
+
+export default Footer
