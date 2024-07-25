17d1186102716c015f771746dea4918c4ff01c7f
oykuky <oykuky001@gmail.com>
2024-07-26 01:12:46 +0300

    added

---
+++ src/service/GlobalApi.jsx
@@ -0,0 +1,17 @@
+import axios from "axios"
+
+const BASE_URL ='https://places.googleapis.com/v1/places:searchText'
+
+const config={
+    headers:{
+        'Content-Type': 'application/json',
+        'X-Goog-Api-Key': import.meta.env.VITE_GOOGLE_PLACES_API_KEY,
+        'X-Goog-FieldMask': [
+            'places.photos',
+            'places.displayName',
+            'places.id'
+        ]
+    }
+}
+
+export const GetPlaceDetails=(data) =>axios.post(BASE_URL,data,config)
