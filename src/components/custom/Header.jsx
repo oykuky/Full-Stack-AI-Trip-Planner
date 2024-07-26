0000cb4ac99a984e11a38f8053e16958a62f00b1
oykuky <oykuky001@gmail.com>
2024-07-26 17:39:52 +0300

    updated

--- src/components/custom/Header.jsx
+++ src/components/custom/Header.jsx
@@ -1,17 +1,51 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover"
+import { googleLogout, useGoogleLogin } from '@react-oauth/google';
+import { useNavigate } from 'react-router-dom';
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { FcGoogle } from "react-icons/fc";
+import axios from 'axios';
 
 
 function Header() {
   const user=JSON.parse(localStorage.getItem('user'));
+  const [openDialog,setOpenDialog]=useState(false);
+
+  
   useEffect(() =>{
     console.log(user)
   },[])
+
+  const login=useGoogleLogin({
+    onSuccess:(codeResp)=>GetUserProfile(codeResp),
+    onError:(error)=>console.log(error)
+  })
+  
+  const GetUserProfile=(tokenInfo)=>{
+    axios.get(`https://www.googleapis.com/oauth2/v1/userinfo?acess_token=${tokenInfo?.access_token}`,{
+      headers: {
+       Authorization: `Bearer ${tokenInfo?.access_token}`,
+       Accept:'Application/json'
+      }
+    }).then((resp) => {console.log(resp);
+      localStorage.setItem('user',JSON.stringify(resp.data));
+      setOpenDialog(false);
+      window.location.reload();
+    })
+  }
+
   return (
     <div className='p-3 shadow-sm flex justify-between items-center px-4'>
       <img src='/logo.svg'/>
@@ -23,15 +57,41 @@ function Header() {
           <PopoverTrigger>
             <img src={user?.picture} className='rounded-full w-[38px] h-[38px]' />
           </PopoverTrigger>
-          <PopoverContent>Place content for the popover here.</PopoverContent>
+          <PopoverContent>
+            <h2 className="cursor-pointer" onClick={()=>{
+              googleLogout();
+              localStorage.clear();
+              window.location.reload();
+            }}>Logout</h2>
+           
+          </PopoverContent>
          </Popover>
 
        </div>
-       : <Button>Sign In</Button> 
+       : <Button onClick={()=>setOpenDialog(true)}>Sign In</Button> 
        }
       </div>
+      
+      <Dialog open={openDialog}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogDescription>
+              <img src="/logo.svg"/>
+              <h2 className="font-bold text-lg mt-6">Sign In with Google</h2>
+              <p>Sign In to the App with Google authentication securely</p>
+              <Button 
+              onClick={login} className="w-full mt-5 flex gap-4 items-center">
+                <FcGoogle className="h-7 w-7"/>
+                Sign In With Google
+              </Button>
+            </DialogDescription>
+          </DialogHeader>
+        </DialogContent>
+      </Dialog>
+
     </div>
   )
 }
 
 export default Header
+//3.14
